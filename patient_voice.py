9c43aa36a0a3a2625580977e2cc0323a0535fedf
TSK208 <srikanth.tadepalli208@gmail.com>
2025-09-12 19:24:32 +0530

    update

--- patient_voice.py
+++ patient_voice.py
@@ -41,10 +41,10 @@ def record_audio(file_path, timeout=20, phrase_time_limit=None):
     except Exception as e:
         logging.error(f"An error occurred: {e}")
 
-audio_filepath="saved_files/patient_voice_test_for_patient.mp3"
+audio_filepath = "saved_files/patient_voice_test_for_patient.mp3"
 #record_audio(file_path=audio_filepath)
 
-#Step2: Setup Speech to text–STT–model for transcription
+# Setup Speech to text–STT–model for transcription
 import os
 from groq import Groq
 
